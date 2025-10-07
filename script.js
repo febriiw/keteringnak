2b3e393254aa13d23191a9f7b91459a022b51f46
febriiw <febrivalentina37@gmail.com>
2025-10-07 12:06:10 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+/1 pilih tmbol berdasarkan ID-nya
+const tombolsapa = document.gtElementBy1d(*sapaButton)
+//2 Tambahkan 'event listener' untuk aksi 'clik
+tombolSapa addEventlistener('click'.function(){
+//3 Jalankan kode ini ketika tombl di-kiri
+alert(hallo!Terima kasih sudah berkunjung!')
+}):
